c37bcb90e7f5df0f2469840b1c1f52ce636200e6
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-02-05 13:07:38 -0500

    Declares first variable

---
+++ Java-Test-Project/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // declare the variable 
+            //format:  <type> <name>
+            int age;
+        
+        // intialize the variable/assign a value to it
+            // format: <name> = <value>
+            age = 28;
+            
+            // print the variable value to the terminal
+            System.out.println("I am " + age + " years old.");
+
+    }
+}
