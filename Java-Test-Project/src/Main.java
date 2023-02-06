e215364b0b39f34ccbe1a5490b75c156313a86e4
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-02-06 14:01:43 -0500

    .

--- Java-Test-Project/src/Main.java
+++ Java-Test-Project/src/Main.java
@@ -2,14 +2,18 @@ public class Main {
     public static void main(String[] args) {
         // declare the variable 
             //format:  <type> <name>
-            final int age;
+            // final int age;
         
         // intialize the variable/assign a value to it
             // format: <name> = <value>
-            age = 28;
+            // age = 28;
             
             // print the variable value to the terminal
-            System.out.println("I am " + age + " years old.");
+            // System.out.println("I am " + age + " years old.");
 
+            char smallLetter = 'a';
+            char capitalLetter = 'A';
+
+            System.out.println(smallLetter > capitalLetter);
     }
 }
