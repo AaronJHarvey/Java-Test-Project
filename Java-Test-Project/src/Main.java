55939885b3d0052a5a24f5c7af480bea13e5c196
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-02-08 13:17:06 -0500

    Contains() and replace()

--- Java-Test-Project/src/Main.java
+++ Java-Test-Project/src/Main.java
@@ -19,8 +19,26 @@ public class Main {
 
             // System.out.println(smallLetter > capitalLetter);
 
-            String name = "Aaron James Harvey";
+            // String name = "Aaron James Harvey";
 
-            System.out.println(Arrays.toString(name.split(" ")));
+            // System.out.println(Arrays.toString(name.split(" ")));
+
+
+
+            // Tests replace() method to replace part of a string - 
+                // String sentence = "This is a test sentence.";
+                // System.out.println(sentence);
+                // String changedSentence = sentence.replace("test sentence", "the same test sentence but it has been changed");
+                // System.out.println(String.format(changedSentence));
+
+            
+
+                // Contains(method) to see if a string contains a certain substring:
+                    String lyric = "Roses are red, violets are blue";
+                    if (lyric.contains("Blue")) {
+                        System.out.println("The lyric has the word blue in it.");
+                    } else {
+                        System.out.println("The lyric does not contain the word blue.");
+                    }
     }
 }
