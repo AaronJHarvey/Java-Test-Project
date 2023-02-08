487c223f535bfe45319ceb6d983734cc5ab1f096
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-02-08 14:10:19 -0500

    Creates a Scanner file and program

---
+++ Java-Test-Project/src/ScannerFile.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class ScannerFile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What's your name?");
+        String name = scanner.nextLine();
+
+
+        System.out.printf("So %s. How old are you?", name);
+        int age = scanner.nextInt();
+
+        System.out.printf("Cool! %d is a good age to start programming!", age);
+        
+        scanner.close();
+    }
+    
+}
